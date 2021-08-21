702a3df2baed5eda65bc51b51d4d99f6da05a3ad
MarioAndrade <yomariolalo@hotmail.com>
2021-08-21 08:29:35 -0500

    Complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,6 @@ import React from "react";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 import Exercise from "./screens/Exercise";
-
-// class App extends React.Component {
-//   render() {
-//     return(
-//       <ApolloProvider client={client}>
-//         <Exercise />
-//       </ApolloProvider>
-
-//     )
-//   }
-// }
-// export default App;
-
 function App() {
   const client = new ApolloClient({
     uri: "http://localhost:3001/graphql",
